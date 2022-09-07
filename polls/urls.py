f8d124f2cff8c40380447428a981f6dbe6312ab5
Dawid <jedrzejczakdaw@gmail.com>
2022-09-07 13:28:41 +0200

    added prometheus

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('hello', views.index, name='index'),
+    path('goodbye', views.goodbye, name='goodbye'),
+
 ]
